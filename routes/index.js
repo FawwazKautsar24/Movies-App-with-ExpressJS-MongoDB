08ed719daaf349965dae03e053a666ac79f91331
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-01-04 02:19:20 +0700

    Konfigurasi Routes Index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET welcome page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('welcome', { title: 'Welcome Page' });
+});
+
+/* GET dashboard page. */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard Page' });
 });
 
 module.exports = router;
